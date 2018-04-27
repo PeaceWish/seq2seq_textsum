7ab7b80917c76fcfd00c40439a4eee0b6e11568e
Peacewish <jyuheng@hotmail.com>
2018-04-27 12:10:31 +0800

    model first

---
+++ seq2seqModel.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/4/27 12:09
+# @Author  : Zhangyuheng
+# @File    : seq2seqModel.py
+
+class seq2seqModel():
+    def __init__(self):
+        pass
